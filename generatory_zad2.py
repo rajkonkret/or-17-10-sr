071a35a4c5a2ca5f44c80b756ff02d9f03d13158
raj <rajkonkret660@gmail.com>
2023-10-18 15:09:46 +0200

    2 - update gen2

--- generatory_zad2.py
+++ generatory_zad2.py
@@ -19,6 +19,16 @@ def fibo():
         a, b = b, a + b
 
 
+def fibo_with_index(n):
+    a, b = 0, 1
+    for idx in range(n):
+        yield idx, a
+        a, b = b, a + b
+
+
+# x = idx, a - tupla(krotka)
+
+
 g = generator2()
 print(next(g))
 print(next(g))
@@ -31,7 +41,6 @@ print(next(g))
 print(next(g))
 print(next(g))
 print(next(g))
-
 
 fib = fibo()
 print(next(fib))
@@ -48,3 +57,32 @@ print(next(fib))
 
 fib2 = fibo()
 print(next(fib2))
+# 14:55
+
+for i in fibo_with_index(10):
+    print(f"element: {i} ")  # element: (9, 34)
+
+for i, n in fibo_with_index(10):
+    print(f"Pozycja: {i}: {n}")
+
+
+def counter(start=0):
+    n = start
+    while True:
+        result = yield n
+        print(result)
+        if result == 'q':
+            break
+        n += 1
+
+
+c = counter(10)
+print(next(c))
+print(next(c))
+print(next(c))
+print(c.send('OK'))  # OK
+try:
+    print(c.send('q'))
+# print(next(c))  # StopIteration
+except StopIteration:
+    print("Generator zakończył działanie")
