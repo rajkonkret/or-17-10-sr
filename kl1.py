b5d8f5c960a036478fc913abec0a15e0e48899ab
raj <rajkonkret660@gmail.com>
2023-10-19 08:53:25 +0200

    2 - add hypot doc to kl1

--- kl1.py
+++ kl1.py
@@ -2,6 +2,22 @@
 # nadanie konkretnych cech  - obiekt
 import math
 
+"""
+hypot(*coordinates) -> value
+
+Multidimensional Euclidean distance from the origin to a point.
+
+Roughly equivalent to:
+    sqrt(sum(x**2 for x in coordinates))
+
+For a two dimensional point (x, y), gives the hypotenuse
+using the Pythagorean theorem:  sqrt(x*x + y*y).
+
+For example, the hypotenuse of a 3/4/5 right triangle is:
+
+    >>> hypot(3.0, 4.0)
+    5.0
+"""
 
 class MyFirstClass:
     """
